8916765883dda52a2c5c64282d2827a6cdc75a08
Lemon Pecan Pie <aidanhpaul@gmail.com>
2025-08-31 08:59:19 -0400

    Finish 2.3

--- src/App.jsx
+++ src/App.jsx
@@ -37,12 +37,8 @@ const Content = ({ parts }) => {
   );
 }
 
-function sumArray(arr) {
-  return arr.reduce((a, b) => a + b, 0);
-}
-
 const Total = ({parts}) => {
-  let exercisesSum = sumArray(parts.map(part => part.exercises));
+  let exercisesSum = parts.reduce((a, b) => a + b.exercises, 0);
   return (
     // <>
     //   <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
